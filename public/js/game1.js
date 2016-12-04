71517e36b64a8a38a3654af1d35bca737d8d21d6
Max Verbeek <m4xv3rb33k@gmail.com>
2016-12-04 17:15:28 +0100

    betere engine die geen jQuery collision nodig heeft (wordt nog wel gebruikt voor 3x3 hitbox om shit te highlighten)

--- public/js/game1.js
+++ public/js/game1.js
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var board, player, hitbox, objective, goal, list, helper;
+var board, player, hitbox, objective, goal, list, helper, finished;
 
 $(function(){
 	board = $('div#board');
@@ -34,7 +34,15 @@ $(function(){
 
 	objective = 0;
 
+	finished = false;
+
 	$(window).keydown(function(e) {
+
+		// stop met bewegen als het level gehaald is.
+		// Dit return statement kan er voor zorgen dat je geen W, A, S of D in kan
+		// typen nadat het level gehaald is (in bijvoorbeeld een highscore ding)
+		// maar dat heb ik niet getest lol
+		if (finished) return;
 
 		switch(e.keyCode) {
 			// W or Arrow up
@@ -86,15 +94,66 @@ function movePlayer(p, dx, dy) {
 	xnew = xold + dx;
 	ynew = yold + dy;
 
-	if (outOfBounds(xnew, ynew, map) || collision(xnew, ynew, map)) return; 
+	// Deze "collide*" closures zijn functies die worden geroepen dmv collideIf.
+	// Hier kun je een character of regex geven, en dan zoekt die functie uit
+	// of de speler collide met dat ding dat met de gegeven string of regex overeenkomt.
+	// Als dit zo is wordt de closure uitgevoerd zodat nog verder gecontroleerd
+	// kan worden of de speler uberhaubt kan colliden met dit ding.
+	// return True als dat kan (wel collision, dus kan er niet op staan), False
+	// als dat niet kan (geen collision -> kan er wel op staan.)
+	var collideExit = function (x, y) {
+		if (objective === goal) {
+			alert('Je hebt het level gehaald!');
+			finished = true;
+			return false;
+		}
 
+		alert('Je hebt nog niet alle snippets code opgepakt. :c');
+
+		return true;
+	};
+
+	var collideObjective = function (x, y) {
+		// Kut manier om het n-de element uit het bord te halen (waar de speler naartoe gaat.)
+		var item = $('#board .tile:eq(' + (y * (map.length - 1) + x) + ')');
+
+		console.log(item);
+
+		// Als dit item nog niet opgepakt moet worden
+		if (item.data('order') > objective) {
+			alert('Je moet eerst nog andere snippets oppakken voordat deze past.');
+
+			return true;
+		}
+
+		// Als dit het item is dat opgepakt moet worden (vanaf hier kun je er doorheen lopen)
+		else if (item.data('order') === objective) {
+			// Dit item moet opgepakt worden.
+			objective++;
+			list.append($('<li></li>').text(item.data('snippet')));
+			item.removeClass('objective').addClass('grass');
+		}
+
+		// Als order < objective, dan is het item al opgepakt, en
+		// kun je er gewoon doorheen lopen.
+
+		return false;
+	};
+
+	// Ziek if-statement lol
+	if (outOfBounds(xnew, ynew, map)
+		|| collideWith(xnew, ynew, map, '#')
+		|| collideIf(xnew, ynew, map, /\d/, collideObjective)
+		|| collideIf(xnew, ynew, map, '@', collideExit)) return;
+
+	// Verander de coordinaten van de speler.
 	p.data('x', xnew);
 	p.data('y', ynew);
 
 	p.css({left: xnew * 32, top: ynew * 32});
 
-	console.log({x: xnew, y: ynew, dx: dx, dy: dy});
 
+	// Regel de 3x3 hitbox om de speler heen (voor het highlighten van objectives)
 	var nearby;
 
 	// Hitbox = 3x3 around player
@@ -105,11 +164,17 @@ function movePlayer(p, dx, dy) {
 	}
 
 	else {
+		// Als er geen objectives naast de speler meer zijn, haal alle highlights weg.
+		// Dit gaat kapot als er meerdere objectives tegelijkertijd in de hitbox kunnen zijn.
+		// Zorg er daarom ook voor dat er altijd maar 1 objective tegelijkertijd
+		// naast de speler kan zijn.
 		$('.board .tile.objective').removeClass('highlighted');
 		helper.text('-')
 	}
+
 }
 
+// Out of bounds controle
 function outOfBounds(x, y, map) {
 	var ymax = map.length - 1;
 	var xmax = map[0].length - 1;
@@ -117,6 +182,20 @@ function outOfBounds(x, y, map) {
 	return x > xmax || y > ymax || x < 0 || y < 0;
 }
 
-function collision(x, y, map) {
-	return map[y][x] == '#'; // wall
+// Als iets collide met een (string of regex) op de map zoals die in
+// resources staat. Regex kan gebruikt worden voor objectives (\d voor getal)
+// string kan gebruikt worden voor exit, wall (@ of # respectievelijk) etc..
+function collideWith(x, y, map, condition) {
+	if (typeof condition === 'string') {
+		return map[y][x] === condition;
+	}
+
+	return condition.test(map[y][x]);
+}
+
+// Handige functie om collision te doen, met logic die ernaast uitgevoerd kan
+// worden. Zo kunnen we dus een exit alleen laten colliden als je nog niet
+// alles hebt opgepakt.
+function collideIf(x, y, map, condition, closure) {
+	return collideWith(x, y, map, condition) && closure(x, y);
 }
